6a864d907bde109d03f0cd11be735256a13a9173
AtakanKorez <atakankorez@gmail.com>
2023-02-19 20:40:16 +0100

    Add project files.

---
+++ EfCore.ExtensionMethod/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfCore.ExtensionMethod
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-DIRE4RG;Initial Catalog=TestApp;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+    }
+}
---
+++ EfCore.ExtensionMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using EfCore.ExtensionMethod;
+
+var context = new AppDbContext();
+
+string nameFilter = "testName";
+string descriptionFilter = "testDescription";
+
+var products = context.Products
+    .WhereIf(!string.IsNullOrEmpty(nameFilter), p => p.Name.Contains(nameFilter))
+    .WhereIf(!string.IsNullOrEmpty(descriptionFilter), p => p.Name.Contains(descriptionFilter)).ToList();
+
+Console.ReadLine();
