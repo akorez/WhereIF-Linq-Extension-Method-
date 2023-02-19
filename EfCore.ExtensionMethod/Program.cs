45243fbedc43c4439c68433d9d0ca3a0998d75c1
AtakanKorez <atakankorez@gmail.com>
2023-02-19 20:54:23 +0100

    Fix bug

--- EfCore.ExtensionMethod/Program.cs
+++ EfCore.ExtensionMethod/Program.cs
@@ -7,6 +7,6 @@ string descriptionFilter = "testDescription";
 
 var products = context.Products
     .WhereIf(!string.IsNullOrEmpty(nameFilter), p => p.Name.Contains(nameFilter))
-    .WhereIf(!string.IsNullOrEmpty(descriptionFilter), p => p.Name.Contains(descriptionFilter)).ToList();
+    .WhereIf(!string.IsNullOrEmpty(descriptionFilter), p => p.Description.Contains(descriptionFilter)).ToList();
 
 Console.ReadLine();
